d3f6f3910275544688505ef1b7c1e09824928c0b
mrakashkumar <akash.ku9596@gmail.com>
2026-03-19 00:35:32 +0800

    added security-application-feature-permission

---
+++ src/main/java/com/akash/authSecurityPermission/AuthSecurityPermissionApplication.java
@@ -0,0 +1,13 @@
+package com.akash.authSecurityPermission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSecurityPermissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSecurityPermissionApplication.class, args);
+	}
+
+}
